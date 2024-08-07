9516bfc17265bfc64e29001e22deb0ddf7bc0d18
syaloni06 <syalonib@gmail.com>
2024-08-07 17:48:48 +0530

    commit for creating form for taking the input values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'b': '0 0 20px 1px rgba(0,0,0,0.1)',
+        '3xl': '0 0 40px 3px rgba(0,0,0,0.1)',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      boxShadow: ['hover'],
+    },
   },
   plugins: [],
 }
